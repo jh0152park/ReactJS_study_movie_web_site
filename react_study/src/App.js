207adf01cf1abf444790c283ab8ac4523e6948ed
jh0152park <jh0152park@naver.com>
2023-06-23 23:34:27 +0900

    added module css files

--- react_study/src/App.js
+++ react_study/src/App.js
@@ -1,9 +1,10 @@
 import Button from "./Button";
+import styles from "./App.module.css";
 
 function App() {
   return (
     <div>
-      <h1>Welcome start to react!</h1>
+      <h1 className={styles.title}>Welcome start to react!</h1>
       <Button text={"Click Me"} />
     </div>
   );
