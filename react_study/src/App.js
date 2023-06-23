fd6a263cf46b4a92527d82801c1a6b5c146d7d19
jh0152park <jh0152park@naver.com>
2023-06-23 23:15:01 +0900

    created new react app

---
+++ react_study/src/App.js
@@ -0,0 +1,12 @@
+import Button from "./Button";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome start to react!</h1>
+      <Button text={"Click Me"} />
+    </div>
+  );
+}
+
+export default App;
